c47d792ec478b02e28b4b5b3b3ed5dc582564eee
antoniayuliana <deruyuliana27@gmail.com>
2024-03-26 12:57:53 +0700

    contoh1.php

---
+++ controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> perkenalkan </h1>";
+        echo "Nama Saya Antonia Yuliana Deru
+        tinggal di daerah bekasi 
+        olahraga yang saya sukai volly";
+    }
+}
